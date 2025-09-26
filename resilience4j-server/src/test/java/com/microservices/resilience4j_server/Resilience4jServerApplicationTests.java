8d7ce4d3319f58590489087d496c7eb1407f4832
Nasir Bhakshey <nasirbhakshey@gmail.com>
2025-09-26 20:41:28 +0530

    first commit

---
+++ resilience4j-server/src/test/java/com/microservices/resilience4j_server/Resilience4jServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.resilience4j_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
