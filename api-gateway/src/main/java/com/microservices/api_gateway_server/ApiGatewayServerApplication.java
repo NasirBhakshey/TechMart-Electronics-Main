d602f1d487b8b2ec7daf6f2c7fea106eb84a75cc
Nasir Bhakshey <nasirbhakshey@gmail.com>
2025-10-07 13:17:59 +0530

    Finally APi Route

--- api-gateway/src/main/java/com/microservices/api_gateway_server/ApiGatewayServerApplication.java
+++ api-gateway/src/main/java/com/microservices/api_gateway_server/ApiGatewayServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableWebFlux
 public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
